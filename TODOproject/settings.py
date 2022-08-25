a998d6283058cba0f1e6f98d24437cb88c2c6a44
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2022-08-24 22:51:00 -0400

    actividad aux 2

--- TODOproject/settings.py
+++ TODOproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import mimetypes
+import todoapp
 
 mimetypes.add_type("text/css", ".css", True)
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'categorias',
+    'todoapp',
 ]
 
 MIDDLEWARE = [
